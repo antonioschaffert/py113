909e16050bcc05f3eabb278fb149cbbbece5d9d2
Tony <antonio.schaffert@gmail.com>
2021-12-15 20:34:40 -0500

    Added new endpoint and updated Readme

--- main.py
+++ main.py
@@ -119,6 +119,10 @@ async def random_joke():
 async def joke_of_the_year():
     return JOKES[0]
 
+@app.get("/number2") 
+async def number2():
+    return JOKES[2]
+
 @app.get("/dumbest") 
 async def dumbest():
     return JOKES[-1]
